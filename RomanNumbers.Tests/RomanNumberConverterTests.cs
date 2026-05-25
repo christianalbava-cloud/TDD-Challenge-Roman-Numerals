099dfef92ee2f7e0a6ae483dc65277540986a9ba
christian.alba.va <christian.alba.va@assuresoft.com>
2026-05-25 17:52:17 -0400

    test: add failing test for numbers 4,9,40,90,400,900

--- RomanNumbers.Tests/RomanNumberConverterTests.cs
+++ RomanNumbers.Tests/RomanNumberConverterTests.cs
@@ -25,4 +25,65 @@ public class RomanNumberConverterTests
 
         result.Should().Be("V");
     }
+
+    [Fact]
+    public void Should_Convert_Number_4_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(4);
+
+        result.Should().Be("IV");
+    }
+
+    [Fact]
+    public void Should_Convert_Number_9_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(9);
+
+        result.Should().Be("IX");
+    }
+
+    [Fact]
+    public void Should_Convert_Number_40_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(40);
+
+        result.Should().Be("XL");
+    }
+
+    [Fact]
+    public void Should_Convert_Number_90_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(90);
+
+        result.Should().Be("XC");
+    }
+
+    [Fact]
+    public void Should_Convert_Number_400_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(400);
+
+        result.Should().Be("CD");
+    }
+    [Fact]
+    public void Should_Convert_Number_900_To_Roman_Number()
+    {
+        var converter = new RomanNumberConverter();
+
+        var result = converter.ToRoman(900);
+
+        result.Should().Be("CM");
+    }
+
+
 }
