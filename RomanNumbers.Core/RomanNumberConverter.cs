7e9bf0623b92341f839e06c594ec142987632d59
christian.alba.va <christian.alba.va@assuresoft.com>
2026-05-25 17:55:58 -0400

    feat: all the test pass successful without refactor

--- RomanNumbers.Core/RomanNumberConverter.cs
+++ RomanNumbers.Core/RomanNumberConverter.cs
@@ -10,6 +10,24 @@ public class RomanNumberConverter
         if (number == 5)
             return "V";
 
+        if (number == 4)
+            return "IV";
+
+        if (number == 9)
+            return "IX";
+
+        if (number == 40)
+            return "XL";
+
+        if (number == 90)
+            return "XC";
+
+        if (number == 400)
+            return "CD";
+
+        if (number == 900)
+            return "CM";
+
         return "";
     }
 }
